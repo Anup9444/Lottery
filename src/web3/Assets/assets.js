c36585aac00d5ce73af1a22f0df7bfae88f2963d
anup9444 <anup@niural.com>
2023-06-02 15:48:25 +0545

    final project

--- src/web3/Assets/assets.js
+++ src/web3/Assets/assets.js
@@ -1,18 +1,9 @@
 export const Assets = {
-  80001: {
-    networkName: "polygonmumbai",
-    isMainnet: false,
-    lotteryPaycontract: "0x78d77E5CdcD1221a6bD598BA09e96F9cE43f0Ea4", // correct
-    blockExplorer: "https://mumbai.polygonscan.com/tx/",
-    USDTTokenAddress: "0x8bf8210d92Df678f61AaBFd561f0B1F83BA7A3fC",
-    decimal: 6,
-  },
-
   //BSC chain
   56: {
     networkName: "BSC",
     isMainnet: true,
-    lotteryPaycontract: "0x", // need to deploy
+    lotteryPaycontract: "0xf69A140123772ACE64f52dd74e202E5C1aDD44b6", // need to deploy
     blockExplorer: "https://bscscan.com/",
     USDTTokenAddress: "0x55d398326f99059fF775485246999027B3197955",
     decimal: 18,
